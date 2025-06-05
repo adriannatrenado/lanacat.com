e7d2e5eb6433e34c9bac7dd15ef5e3e6fe67fadf
adrianna trenado <adri@adriannas-MacBook-Air.local>
2025-06-05 02:24:09 -0700

    add music button

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>
-    const cursor = document.getElementById("custom-cursor");
-    document.addEventListener("mousemove", function(e) {
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
-    });
-</script>
